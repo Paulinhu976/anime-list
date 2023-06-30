758ae1312b1da66ac59cef54784ce8db35ac606f
Paulo Felipe <p_felipes@hotmail.com>
2023-06-30 11:04:10 -0300

    feat: creat cruds and emulate bd

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,75 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import fs from 'fs';
+
+const port = 3000;
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 4001;
+
+let users: IUser[] = [];
+
+interface IUser {
+  id: string;
+  userName: string;
+  email: string;
+  password: string;
+  birthday: Object;
+}
+
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
-  res.json({
-    message: 'pong',
+fs.readFile('db.json', 'utf-8', (err, data) => {
+  if (err) {
+    console.log('erro');
+  } else {
+    users = JSON.parse(data);
+  }
+});
+
+app.post('/user', (req: Request, res: Response) => {
+  const { id, userName, email, password, birthday }: IUser = req.body;
+
+  const user: IUser = {
+    id,
+    email,
+    userName,
+    password,
+    birthday,
+  };
+
+  users.push(user);
+
+  fs.writeFile('db.json', JSON.stringify(users), (err) => {
+    if (err) {
+      console.log('erro');
+    } else {
+      console.log('deu bom');
+    }
   });
+  return res.send(users);
+});
+
+app.get('/user', (req: Request, res: Response) => {
+  return res.send(users);
+});
+
+app.delete('/user/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userId = users.findIndex((userId) => userId.id === id);
+  users.splice(userId, 1);
+
+  return res.send('item removido com sucesso');
+});
+
+app.put('/user/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { userName } = req.body;
+  const userId = users.findIndex((userId) => userId.id === id);
+
+  users[userId] = {
+    ...users[userId],
+    userName,
+  };
+
+  res.send('Username alterado com sucesso');
 });
 
 app.listen(port, () => {
